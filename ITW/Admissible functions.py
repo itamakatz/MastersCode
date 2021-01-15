ec0c33d0573603e6ddcbd28b65ba773cc281125c
Itamar Katz <itamakatz@gmail.com>
2021-01-15 15:17:55 +0200

    created plot_normalization_functions
    including all functions for the normalization.
    Next step is to draw a dotted line for x0

--- ITW/Admissible functions.py
+++ ITW/Admissible functions.py
@@ -1,21 +1,23 @@
 import numpy as np
 import matplotlib
 import matplotlib.font_manager
-matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+T_STEP = 0.0002
 TICK_SIZE = 30
-# matplotlib.rcParams['legend.fontsize'] = 20
-# matplotlib.rcParams['legend.size'] = 20
+
 matplotlib.rcParams['legend.handlelength'] = 4
 matplotlib.rcParams['xtick.labelsize'] = TICK_SIZE
 matplotlib.rcParams['ytick.labelsize'] = TICK_SIZE
+
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 
 FISHER_COLOR = 'b' #'r'
 FISHER_SQRT_COLOR = 'r' #'m'
 ENTROPY_COLOR = 'g' #'b'
 ERROR_COLOR = 'y' #'g'
-
-T_STEP = 0.0002
 
 def get_fisher(t):
     return np.array(t*(1-t))
@@ -25,6 +27,8 @@ def get_fisher_der2(t):
     return np.array(list(map(lambda x: -2, t)))
 def get_fisher_ratio(t):
     return np.array(get_fisher_der2(t)/get_fisher_der1(t))
+def get_fisher_normalization(t, x0):
+    return get_normalization(t, get_fisher, get_fisher_der1, x0)
 
 def get_fisherSqrt(t):
     return np.array((t*(1-t))**0.5)
@@ -34,6 +38,8 @@ def get_fisherSqrt_der2(t):
     return np.array(-1/(4*(t*(1-t))**1.5))
 def get_fisherSqrt_ratio(t):
     return np.array(get_fisherSqrt_der2(t)/get_fisherSqrt_der1(t))
+def get_fisherSqrt_normalization(t, x0):
+    return get_normalization(t, get_fisherSqrt, get_fisherSqrt_der1, x0)
 
 def get_entropy(t):
     t = t[1:-1]
@@ -44,6 +50,8 @@ def get_entropy_der2(t):
     return np.array(1/(t*(t-1)))
 def get_entropy_ratio(t):
     return np.array(get_entropy_der2(t)/get_entropy_der1(t))
+def get_entropy_normalization(t, x0):
+    return get_normalization(t, get_entropy, get_entropy_der1, x0)
 
 def get_error(t):
     return np.array([t, 1-t]).min(axis=0)
@@ -53,6 +61,15 @@ def get_error_der2(t):
     return np.array(list(map(lambda x: 0, t)));
 def get_error_ratio(t):
     return np.array(get_error_der2(t)/get_error_der1(t))
+def get_error_normalization(t, x0):
+    return get_normalization(t, get_error, get_error_der1, x0)
+
+def get_normalization(xs, func, func_der1, x0):
+    x0_index = get_index_of_value(xs, x0)
+    func_ys = func(xs)
+    func_at_x0 = func_ys[x0_index]
+    func_der1_at_x0 = func_der1(xs)[x0_index]
+    return np.array((func_ys-func_at_x0) / func_der1_at_x0 +x0)
 
 def log(arr):
     return np.log(np.abs(np.array(arr)))
@@ -60,19 +77,17 @@ def log(arr):
 def get_min_arrays(arrays, factor = 1):
     minY = np.array(arrays).min(axis=0).min()
     return minY * factor
-
 def get_max_arrays(arrays, factor = 1):
     maxY = np.array(arrays).max(axis=0).max()
     return maxY * factor
 
+def get_index_of_value(array, val):
+    return min(range(len(array)), key=lambda i: abs(array[i]-val))
+
 def new_figure():
     plt.figure(num=None, figsize=(16, 12), dpi=80)
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-
-def print_functions():
+def plot_admissible_functions():
 
     t = np.append(np.arange(0., 1., T_STEP),1)
     print("len t: " + str(len(t)))
@@ -102,7 +117,6 @@ def print_functions():
     # plt.rc('font', weight='bold')
 
     # plt.rcParams['font.size'] = '30'
-    # leg = plt.legend(fontsize=30, markerscale=2)
     leg = plt.legend()
     leg_lines = leg.get_lines()
     leg_texts = leg.get_texts()
@@ -113,7 +127,45 @@ def print_functions():
     plt.savefig('Admissible Functions.png')
     plt.show()
 
-def print_functions_der1():
+def plot_normalization_functions():
+
+    t = np.append(np.arange(0., 1., T_STEP),1)
+    print("len t: " + str(len(t)))
+
+    x0 = 0.3
+
+    fisher = get_fisher_normalization(t, x0)
+    fisherSqrt = get_fisherSqrt_normalization(t, x0)
+    entropy = get_entropy_normalization(t, x0)
+    error = get_error_normalization(t, x0)
+
+    new_figure()
+    
+    LINEWIDTH = 5
+    FONT_SIZE = 25
+    plt.plot(t, fisher, FISHER_COLOR, label="Fisher", linewidth=LINEWIDTH)
+    plt.plot(t, fisherSqrt, FISHER_SQRT_COLOR, label="Root-Fisher" , linewidth=LINEWIDTH)
+    plt.plot(t, entropy, ENTROPY_COLOR, label="Binary Entropy", linewidth=LINEWIDTH)
+    plt.plot(t, error, ERROR_COLOR, label="Error Probability" , linewidth=LINEWIDTH)
+
+    # plt.title('Admissible Functions on [0,1]', fontsize=FONT_SIZE)
+    plt.xlabel(r'$\boldsymbol{p}$', fontsize=FONT_SIZE)
+    plt.ylabel(r'$\boldsymbol{g_{p_0}(p)}$', fontsize=FONT_SIZE)
+
+    plt.ylim(-0.6, get_max_arrays([fisher, fisherSqrt, entropy, error], 1.05))
+
+    leg = plt.legend()
+    leg_lines = leg.get_lines()
+    leg_texts = leg.get_texts()
+    plt.setp(leg_lines, linewidth=4)
+    plt.setp(leg_texts, fontsize=30)
+    # plt.legend(loc="upper left")
+
+    plt.savefig('Normalized Functions.png')
+    plt.show()
+
+def plot_functions_der1():
+
     t = np.arange(0., 1., T_STEP)
     t = t[1:]
     print("len t: " + str(len(t)))
@@ -142,7 +194,7 @@ def print_functions_der1():
     plt.savefig('First derivatives.png')
     plt.show()
     
-def print_functions_der2():
+def plot_functions_der2():
     t = np.arange(0., 1., 0.0002)
     t = t[1:]
     print("len t: " + str(len(t)))
@@ -171,7 +223,7 @@ def print_functions_der2():
     plt.savefig('Second derivatives.png')
     plt.show()
     
-def print_functions_der_ratio():
+def plot_functions_der_ratio():
     t = np.arange(0., 0.5, T_STEP)
     t = t[1:]
     print("len t: " + str(len(t)))
@@ -193,7 +245,7 @@ def print_functions_der_ratio():
     plt.savefig('Too concave condition - Log of Ratio.png')
     plt.show()  
 
-def print_functions_ratio_difference():
+def plot_functions_ratio_difference():
 
     t = np.arange(0., 0.5, T_STEP)
     t = t[1:]
@@ -220,7 +272,7 @@ def print_functions_ratio_difference():
     plt.savefig('Too concave condition - Difference log ratio.png')
     plt.show()
     
-def print_functions_ratio_of_ratios():
+def plot_functions_ratio_of_ratios():
 
     t = np.arange(0., 0.5, T_STEP)
     t = t[1:]
@@ -247,9 +299,10 @@ def print_functions_ratio_of_ratios():
     plt.savefig('Too concave condition - Log ratio of ratios.png')
     plt.show()
 
-print_functions()
-# print_functions_der1()
-# print_functions_der2()
-# print_functions_der_ratio()
-# print_functions_ratio_difference()
-# print_functions_ratio_of_ratios()
+# plot_admissible_functions()
+plot_normalization_functions()
+# plot_functions_der1()
+# plot_functions_der2()
+# plot_functions_der_ratio()
+# plot_functions_ratio_difference()
+# plot_functions_ratio_of_ratios()
